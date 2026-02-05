4ab3a36d7e7863b6ddf9428b185a18a036398b66
Ephra <your.email@example.com>
2026-02-05 20:31:45 +0000

    updated file

--- src/services/email.ts
+++ src/services/email.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://aurys-roses-production.up.railway.app/api'; // Update with your actual backend URL
+const API_BASE_URL = 'https://aurys-roses-staging.up.railway.app/api'; // Update with your actual backend URL
 export const emailApi = axios.create({
   baseURL: API_BASE_URL,
   headers: {
