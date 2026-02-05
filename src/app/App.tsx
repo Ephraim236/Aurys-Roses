30ea95cf7f3f60f13f55e3c5d3c82a2e0d646c8a
Ephra <your.email@example.com>
2026-02-05 20:47:35 +0000

    last update

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -133,7 +133,7 @@ export default function App() {
     setContactLoading(true);
 
     try {
-      const res = await fetch('http://localhost:4001/api/contact',  {
+      const res = await fetch('http://aurys-roses-staging.up.railway.app/api/contact',  {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
